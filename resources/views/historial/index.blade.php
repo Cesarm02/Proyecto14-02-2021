ed60a3fcfb68d0d27ee5e69c435f40eb4617a823
Cesarm02 <cesar-2007-mesa@hotmail.com>
2021-03-29 02:28:47 -0500

    prueba9

--- resources/views/historial/index.blade.php
+++ resources/views/historial/index.blade.php
@@ -60,6 +60,55 @@
   </div>
 </div>
 
+<div class="accordion" id="accordionExample" style="margin-bottom: 10px; padding: 10px;">
+  <div class="card" style="border-radius: 10px;">
+    <div class="card-header" id="heading" style="background-color: #DCDCDC;">
+      <h4 class="mb-0">
+        Medicamentos
+      </h4>
+    </div>
+      <div class="card-header shadow" id="medicamentos" role="button" data-toggle="collapse" href="#collapsemedicamentos" aria-expanded="true" aria-controls="collapseOne" class="trigger collapsed" style="text-decoration: none;">
+        <h5 class="panel-title">
+          <a>
+            Registro de medicamentos
+          </a>
+        </h5>
+      </div>
+    
+      <div id="collapsemedicamentos" class="panel-collapse collapse" aria-labelledby="medicamentos" data-parent="#accordionExample">
+        <div class= "card-body">
+          <table  style="text-align:center"  class="table table-striped table-bordered ">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Cantidad</th>
+                <th>Administracion</th>
+                <th>Horario</th>
+                <th>Estado</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($medicamentos as $medicamento)
+            <tr>
+                <td> {{$medicamento->id}}</td>
+                <td> {{$medicamento->nombre}}</td>
+                <td> {{$medicamento->cantidad}}</td>
+                <td> {{$medicamento->administracion}}</td>
+                <td> {{$medicamento->horario}} horas</td>
+                <td> {{$medicamento->estado}}</td>
+
+            </tr>
+            @endforeach
+        </tbody>
+        
+    </table>
+        </div>
+      </div>
+  </div>
+</div>
+
 </div>
 
 @endsection
