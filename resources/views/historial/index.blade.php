e4d7b9b0501d95b3f45e51511ea6c1da79a15698
Cesarm02 <cesar-2007-mesa@hotmail.com>
2021-03-29 02:27:29 -0500

    prueba8

--- resources/views/historial/index.blade.php
+++ resources/views/historial/index.blade.php
@@ -15,6 +15,51 @@
         <h3> Sección de <strong> Historial </strong> 
         </h3>
     </div>
+@include('personalizar.mensaje')
 
+<div class="accordion" id="accordionExample" style="margin-bottom: 10px; padding: 10px;">
+  <div class="card" style="border-radius: 10px;">
+    <div class="card-header" id="heading" style="background-color: #DCDCDC;">
+      <h4 class="mb-0">
+        Peso y altura
+      </h4>
+    </div>
+      <div class="card-header shadow" id="peso" role="button" data-toggle="collapse" href="#collapsepeso" aria-expanded="true" aria-controls="collapseOne" class="trigger collapsed" style="text-decoration: none;">
+        <h5 class="panel-title">
+          <a>
+            Registro de pesos y alturas
+          </a>
+        </h5>
+      </div>
+    
+      <div id="collapsepeso" class="panel-collapse collapse" aria-labelledby="peso" data-parent="#accordionExample">
+        <div class= "card-body">
+          <table  style="text-align:center;"  class="table  table-striped table-bordered table-responsive">
+        <thead>
+            <th> Fecha</th>
+            <th> Peso</th>
+            <th> Altura</th>
+            <th> IMC </th>
+            <th> Comentario</th>
+        </thead>
+        <tbody>
+            @foreach($pesos as $peso)
+            <tr>
+                <td> {{$peso->fecha}}</td>
+                <td> {{$peso->peso}}</td>
+                <td> {{$peso->altura}}</td>
+                <td> {{$peso->imc}}</td>
+                <td> {{$peso->comentario}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+        
+    </table>
+        </div>
+      </div>
+  </div>
+</div>
+
+</div>
 
 @endsection
