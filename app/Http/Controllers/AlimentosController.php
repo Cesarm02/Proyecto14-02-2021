359afca630d1e5bc96df68fe2545d0690c9cc2c3
Cesarm02 <cesar-2007-mesa@hotmail.com>
2021-03-29 17:01:19 -0500

    Prueba

--- app/Http/Controllers/AlimentosController.php
+++ app/Http/Controllers/AlimentosController.php
@@ -46,40 +46,37 @@ class AlimentosController extends Controller
      */
     public function store(Request $request)
     {
-        // dd("entro");
-        // return redirect()->route('Alimentos.index');
+        $this->authorize('haveaccess', 'alimentos.create');
+
+        $request->validate([
+            'fecha' => 'required',
+            'hora' => 'required',
+            'tipo' => 'required',
+            'descripcion' => 'required'
+        ]);
+        
+        $alimento = ResumenCeg::create([
+            'informacion_user_id' => Auth()->user()->id,
+            'categoria' => 'comida',
+        ] + $request->all());
+
+        // $alimentos = ResumenCeg::create([
+        //     'informacion_user_id' => auth()->user()->id,
+        //     'categoria' => 'comida'
+        // ] + $request->all());
+
+        ControlCambios::create([
+            'fecha_hora' => date('Y/m/d H:i:s', time()),
+            'descripcion' => 'Se crea registro ' . $request->get('id') . ' en la tabla resumen_cegs[Alimentos]',
+            'id_usuario' => Auth()->user()->id
+        ]);
+
+        // return redirect()->route('Alimentos.index')
+        // ->with('status_success', 'Alimento agregado correctamente');
         $usuario = Auth::user()->id;
         $alimentos = ResumenCeg::where('informacion_user_id', $usuario)->where('categoria', 'comida')->get();
         return view('Alimentos.index', compact('alimentos'));
-        // $this->authorize('haveaccess', 'alimentos.create');
-        // dd($request->all());
 
-        // $request->validate([
-        //     'fecha' => 'required',
-        //     'hora' => 'required',
-        //     'tipo' => 'required',
-        //     'descripcion' => 'required'
-        // ]);
-        
-        // $alimento = ResumenCeg::create([
-        //     'informacion_user_id' => Auth()->user()->id,
-        //     'categoria' => 'comida',
-        // ] + $request->all());
-
-        // $alimentos = ResumenCeg::create([
-            //     'informacion_user_id' => auth()->user()->id,
-            //     'categoria' => 'comida'
-            // ] + $request->all());
-            
-        // ControlCambios::create([
-        //     'fecha_hora' => date('Y/m/d H:i:s', time()),
-        //     'descripcion' => 'Se crea registro ' . $request->get('id') . ' en la tabla resumen_cegs[Alimentos]',
-        //     'id_usuario' => Auth()->user()->id
-        // ]);
-        // return view('Alimentos.index');   
-        
-        // return redirect()->route('Alimentos.index')
-        // ->with('status_success', 'Alimento agregado correctamente');
     }
 
     /**
