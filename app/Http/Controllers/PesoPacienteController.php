16818cd117d103d5a6ae4f6bb1db015543960275
Cesarm02 <cesar-2007-mesa@hotmail.com>
2021-03-29 03:01:49 -0500

    prueba4

--- app/Http/Controllers/PesoPacienteController.php
+++ app/Http/Controllers/PesoPacienteController.php
@@ -446,9 +446,10 @@ class PesoPacienteController extends Controller
                 }
             }
 
+            dd($request->all());
 
             $datos = PesoPaciente::create([
-                // 'fecha' => date('Y/m/d H:i:s', time()),
+                'fecha' => date('Y/m/d H:i:s', time()),
                 'imc' => $indice,
                 'informacion_user_id' => auth()->user()->id,
                 'comentario' => $comentario,
